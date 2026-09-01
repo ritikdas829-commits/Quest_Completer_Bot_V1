fb50597343ee71e48782a72c0a029a83f7558402
ritikdas829-commits <ritikdas829@gmail.com>
2026-09-01 09:53:44 +0545

    Update quest.js

--- src/quest/quest.js
+++ src/quest/quest.js
@@ -34,4 +34,22 @@ export class Quest {
     updateUserStatus(userStatus) {
         this.#data.user_status = userStatus;
     }
+
+    // Yeh method check karega ki quest ke andar valid aur supported tasks maujud hain ya nahi
+    isValidQuest() {
+        const tasks = (this.#data.config?.task_config ?? this.#data.config?.task_config_v2)?.tasks;
+        if (!tasks) return false;
+        
+        const supportedTypes = [
+            'PLAY_ON_DESKTOP',
+            'WATCH_VIDEO',
+            'STREAM_ON_DESKTOP',
+            'PLAY_ACTIVITY',
+            'WATCH_VIDEO_ON_MOBILE',
+            'LEARN_MORE',
+            'WATCH_VIDEO_EMBED'
+        ];
+
+        return Object.keys(tasks).some(type => supportedTypes.includes(type));
+    }
 }
